eeee69f377016eb2efa3ffb920ebf63f3e0ded73
Timofey Peshin <timoffex@gmail.com>
2025-02-09 18:52:59 -0800

    add -dev to version, determine it dynamically

--- h2serve/__init__.py
+++ h2serve/__init__.py
@@ -5,6 +5,8 @@ from ._request import DataChunk, Header, HTTP2Request
 from ._response import HTTP2Response
 from ._server import Server, serve
 
+__version__ = "0.1.0-dev"
+
 __all__ = [
     "AppHandler",
     "DataChunk",
